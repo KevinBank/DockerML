8546043bb0c7104893cacef3e147386b8349c908
kevin <kevinb2002@live.nl>
2021-03-31 12:24:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#import libraries
 import requests
 
+#request name and return UUID by contacting mojang API
 def respond():
     print("Name?")
     x=input()
     data=requests.get("https://api.mojang.com/users/profiles/minecraft/{}".format(x)).json()
     print(data['id'])
 
+#call respond
 respond()
